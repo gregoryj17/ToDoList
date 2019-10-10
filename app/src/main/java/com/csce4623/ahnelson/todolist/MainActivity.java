87e3c7c43fa3cc74c350830a5043cb287bf52ba8
gregoryj17 <jaxtaxman@gmail.com>
2019-10-10 11:36:38 -0500

    Initial commit

---
+++ app/src/main/java/com/csce4623/ahnelson/todolist/MainActivity.java
@@ -0,0 +1,13 @@
+package com.csce4623.ahnelson.todolist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
